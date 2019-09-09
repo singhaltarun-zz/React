0e31c782334665fa993da1b02cbca681670ca97c
khandelwal-ankit <ankit.khandelwal@mindtickle.com>
2019-09-09 10:54:09 +0530

    change to titos backend url

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const API_BASE_HOST = process.env.BATON_NODE_REST_API_SERVICE_HOST   || 'http://localhost'
-export const API_BASE_PORT = process.env.BATON_NODE_REST_API_SERVICE_PORT  || '31001'
+export const API_BASE_HOST =  'http://baton-ui-backend.internal.titos.mindtickle.com'
+export const API_BASE_PORT =  '80'
